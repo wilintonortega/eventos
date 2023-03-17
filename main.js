c0a8f129e98e811fe0c0614cc148f1bbf417c25b
wili1991 <wikki9106@gmail.com>
2023-03-17 06:14:30 -0500

    Eventos mediante JavaScript

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
-function hacer() {
-    alert("Hola!, Estimado Usuario");
-
+const boton=document.querySelector("button");
+boton.onclick=function () {
+    alert("Hola, estimado usuario---");
 }
+
+const boton2=document.querySelector(".atributo");
+const hacer=()=>alert("hola con setattribute");
+boton2.setAttribute("onclick","hacer()")
